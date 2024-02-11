b11bab876e06632334eeaf16b49671350fc05819
Amenallah28 <amenallahmrabet9@gmail.com>
2024-02-11 23:02:59 +0100

    betty teee il python

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -5,7 +5,7 @@ BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """ this is the class"""
+    """this is the class"""
 
     def __init__(self, width, height):
         """initialize height and width"""
@@ -20,7 +20,6 @@ class Rectangle(BaseGeometry):
         return self.__width * self.__height
 
     def __str__(self):
-        """ return the following rectangle description:
+        """return the following rectangle description:
         [Rectangle] <width>/<height>"""
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
-    
