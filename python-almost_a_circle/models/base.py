73da24742d9250b3e12e9a32cfb90e2ef327e77f
Amenallah28 <amenallahmrabet9@gmail.com>
2024-02-17 15:25:59 +0100

    writes the json string representation

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -3,8 +3,10 @@
 
 import json
 
+
 class Base:
-    """ belehy ikhdem """
+    """belehy ikhdem"""
+
     __nb_objects = 0
 
     def __init__(self, id=None):
@@ -13,12 +15,20 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-    
+
     @staticmethod
     def to_json_string(list_dictionaries):
-        """static method the returns the json string"""
+        """static method that returns the json string"""
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
     
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ writes the JSON string representation """
+        if list_objs is not None:
+            list_objs = [objs.to_dictionary() for objs in list_objs]
+        with open("{}.json".format(cls.__name__), "w", encoding="utf-8") as f:
+            f.write(cls.to_json_string(list_objs))
     
