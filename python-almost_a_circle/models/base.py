109a133163d6d2edb455b122c949ad1bde792694
Amenallah28 <amenallahmrabet9@gmail.com>
2024-02-17 15:04:13 +0100

    adding static method that returns json string

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """  bismellah : the base class """
 
+import json
 
 class Base:
     """ belehy ikhdem """
@@ -12,3 +13,12 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+    
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """static method the returns the json string"""
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
+    
+    
