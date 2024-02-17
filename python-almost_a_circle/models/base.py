820a830ce9c126003376570ad287d74dea6a0e7b
Amenallah28 <amenallahmrabet9@gmail.com>
2024-02-17 15:57:46 +0100

    return the list of json string representation

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -22,13 +22,20 @@ class Base:
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
-    
 
     @classmethod
     def save_to_file(cls, list_objs):
-        """ writes the JSON string representation """
+        """writes the json string representation"""
         if list_objs is not None:
             list_objs = [objs.to_dictionary() for objs in list_objs]
         with open("{}.json".format(cls.__name__), "w", encoding="utf-8") as f:
             f.write(cls.to_json_string(list_objs))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """returns the list of the json string representation json_string:"""
+        if json_string is None or not json_string:
+            return ([])
+        else:
+            return (json.loads(json_string))
     
