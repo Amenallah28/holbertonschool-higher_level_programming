2f4a51770aff2a010f0f7602bdc955c27ffbea84
Amenallah28 <amenallahmrabet9@gmail.com>
2024-03-19 17:29:09 +0100

    data structures

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list is None or len(my_list) == 0:
+        return None
+    elif len(my_list) == 1:
+        return my_list[0]
+    else:
+        result = my_list[0]
+        for max in range(0, len(my_list) - 1):
+            if result < my_list[max + 1]:
+                result = my_list[max + 1]
+        return result
+    
