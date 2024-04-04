2c851326510d5dcb34709619308ce3887265bfa6
Amenallah28 <amenallahmrabet9@gmail.com>
2024-04-04 13:10:56 +0100

    adding a new item to the list

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
-"""Write a script that adds all arguments to a Python list,
- and then save them to a file"""
-save_to_json_file = __import__("5-save_to_json_file.py").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file.py").load_from_json_file
-import json
+"""Add a new item to the list."""
+from sys import argv
+from os.path import isfile
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-save_to_json_file("add_item.json")
+filename = "add_item.json"
+if isfile(filename):
+    appa = load_from_json_file(filename)
+else:
+    appa = []
+save_to_json_file(list(appa + argv[1:]), filename)
